8faa8fba7a61e13a67e551077031536618796ec1
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-23 16:55:45 -0500

    hlaudvoice.c: new unit (0x800AC330-0x800ACECC), 5 sweep files folded in

--- src/hlaudvoice.c
+++ src/hlaudvoice.c
@@ -1,12 +1,34 @@
-// Small functions found by the sweep (sweep.py). Original file and meanings unknown.
-
 #include "game_types.h"
 
+void fn_800AC330();
+s32 fn_800AC470(void);
+s32 fn_800AC494(void);
+void fn_800AC49C(void);
+s32 fn_800AFCBC(u16, u8);
+void fn_800ACA5C(void* arg0, u8 arg1);
 s32 fn_800B0748();
 s32 fn_800ACA94();
-void fn_800ACA5C(void* arg0, u8 arg1);
-
 void fn_800ACB28(void* arg0);
+f32 fn_800ACEC4(f32 x0, f32 x1);
+
+s32 fn_800AC470(void) {
+    fn_800AC330();
+    return 1;
+}
+
+s32 fn_800AC494(void) {
+    return 1;
+}
+
+void fn_800AC49C(void) {
+}
+
+void fn_800ACA5C(void* arg0, u8 arg1) {
+    if ((arg0 != NULL) && !(((u8) (*(u8*)((u8*)(arg0) + 0xB)) >> 6U) & 1)) {
+        fn_800AFCBC((*(u16*)((u8*)(arg0) + 8)), arg1);
+    }
+}
+
 void fn_800ACB28(void* arg0) {
     fn_800ACA94();
     (*(s32*)((u8*)(arg0) + 0x1C)) = 0;
@@ -20,4 +42,8 @@ void fn_800ACB28(void* arg0) {
             (*(s32*)((u8*)(arg0) + 0x2C)) = 0;
         }
     }
+}
+
+f32 fn_800ACEC4(f32 x0, f32 x1) {
+    return (x0 * x1);
 }
