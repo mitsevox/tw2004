e31d7252a3a0a9c32fce2bbd165d97afdad65d31
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-23 16:50:31 -0500

    ska_shared.c: new unit (0x8001FB00-0x80021ADC), 4 sweep files folded in

--- src/ska_shared.c
+++ src/ska_shared.c
@@ -1,11 +1,27 @@
-// Small functions found by the sweep (sweep.py). Original file and meanings unknown.
-
 #include "game_types.h"
 
+void fn_800B68B4();
+void fn_8001FCA8(s32 p0, s32 p1);
+void fn_800B67EC();
+void fn_8001FCD4(void);
+extern u8 lbl_80281CC0;
+void fn_80021978(u8 v);
 f32 fn_800205F8();
 extern f32 lbl_80282CC0;
-
 f32 fn_80021A98(void* arg0);
+
+void fn_8001FCA8(s32 p0, s32 p1) {
+    fn_800B68B4(p1, p0);
+}
+
+void fn_8001FCD4(void) {
+    fn_800B67EC();
+}
+
+void fn_80021978(u8 v) {
+    lbl_80281CC0 = v;
+}
+
 f32 fn_80021A98(void* arg0) {
     void* temp_r31;
 
