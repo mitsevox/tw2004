2b6ed00005baab9a502bc081836c385535b80f17
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-23 14:23:25 -0500

    delay.c: linked

--- configure.py
+++ configure.py
@@ -470,7 +470,7 @@ config.libs = [
             Object(NonMatching, "src/ax/AXSPB.c"),
             Object(Matching, "src/ax/AXProf.c"),
             Object(NonMatching, "src/axfx/reverb_hi.c"),
-            Object(NonMatching, "src/axfx/delay.c"),
+            Object(Matching, "src/axfx/delay.c"),
             Object(NonMatching, "src/dsp/dsp.c"),
             Object(Matching, "src/dvd/dvdFatal.c"),
             Object(NonMatching, "src/gx/GXDisplayList.c"),
