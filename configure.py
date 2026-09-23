7a653680e8bcaf8833ddad49deebad88d533790a
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-23 10:36:06 -0500

    dolphin/os/OSReset.c linked as Matching

--- configure.py
+++ configure.py
@@ -755,7 +755,7 @@ config.libs = [
             Object(Matching, "dolphin/os/OSAlarm.c"),
             Object(NonMatching, "dolphin/os/OSArena.c"),
             Object(NonMatching, "dolphin/os/OSCache.c"),
-            Object(NonMatching, "dolphin/os/OSReset.c"),
+            Object(Matching, "dolphin/os/OSReset.c"),
             Object(Matching, "dolphin/os/OSRtc.c"),
             Object(Matching, "dolphin/os/OSTime.c"),
             Object(Matching, "dolphin/PPCArch.c"),
