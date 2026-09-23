0ee8d90e87bd285f0c5bc21728be0a589427a59e
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-23 17:14:31 -0500

    DiscCheck.c: new unit (0x8010FF9C-0x801104AC), 4 sweep files folded in

---
+++ src/DiscCheck.c
@@ -0,0 +1,39 @@
+#include "game_types.h"
+
+extern u8 lbl_802824D9;
+void fn_80110178(u8 v);
+s32 DVDGetCurrentDiskID();
+s32 fn_8011027C(void);
+extern u8 lbl_802824D0;
+extern u8 lbl_802824D1;
+u8 fn_80110450(void);
+void fn_80110458(u8 v);
+u8 fn_80110460(void);
+extern u8 lbl_80213B80[];
+u8* fn_801104A0(void);
+
+void fn_80110178(u8 v) {
+    lbl_802824D9 = v;
+}
+
+s32 fn_8011027C(void) {
+    s32 t0;
+    t0 = DVDGetCurrentDiskID();
+    return ((u32)((-*(u8*)(((u8*)t0) + 0x6)) | *(u8*)(((u8*)t0) + 0x6)) >> 31);
+}
+
+u8 fn_80110450(void) {
+    return lbl_802824D0;
+}
+
+void fn_80110458(u8 v) {
+    lbl_802824D1 = v;
+}
+
+u8 fn_80110460(void) {
+    return lbl_802824D1;
+}
+
+u8* fn_801104A0(void) {
+    return lbl_80213B80;
+}
