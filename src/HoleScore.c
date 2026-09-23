3da854d41900d30152a5c8fe3b46c37679ef692b
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-23 16:26:39 -0500

    HoleScore.c: widened to (0x800CF158-0x800D1698), 1 sweep files folded in

--- src/HoleScore.c
+++ src/HoleScore.c
@@ -3,6 +3,26 @@
 #include "golfer.h"
 #include "game.h"
 
+// ---- sweep code (not yet cleaned up) ----
+
+f32 fn_800D05A4(f32* pPos);
+f32 fn_800D04AC(int nPlayer);
+f32 fn_800D04E0(int nPlayer);
+
+f32 fn_800D0478(int nPlayer) {
+    return fn_800D05A4(gPlayers[nPlayer].ball.vPos);
+}
+
+f32 fn_800D04AC(int nPlayer) {
+    return fn_800D05A4(gPlayers[nPlayer].ball.vStart);
+}
+
+f32 fn_800D04E0(int nPlayer) {
+    return fn_800D05A4(gPlayers[nPlayer].ballBefore.vPos);
+}
+
+// ---- end of sweep code ----
+
 // The score the hole will finish on once the tap-in drops: strokes so far plus one, minus par.
 int Hole_ScoreAfterTapIn(int nPlayer) {
     return gPlayers[nPlayer].nStrokes[Game_CurHoleIndex()] + 1 - fn_800D2B08();
