dedf8aa6af96fb670062e70d27ed36475e88c0be
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-23 16:26:29 -0500

    sweep_800D0478: real Player/Ball fields and f32 returns before folding

--- src/unsorted/sweep_800D0478.c
+++ src/unsorted/sweep_800D0478.c
@@ -1,21 +1,19 @@
 // Small functions found by the sweep (sweep.py). Original file and meanings unknown.
 
-#include "game_types.h"
+#include "game.h"
 
-extern u8 gPlayers[];
-void fn_800D05A4();
+f32 fn_800D05A4(f32* pPos);
 
-void fn_800D0478(s32 p0);
-void fn_800D04AC(s32 p0);
-void fn_800D04E0(s32 p0);
-void fn_800D0478(s32 p0) {
-    fn_800D05A4(((gPlayers + (p0 * 3832)) + 0xA90), (p0 * 3832));
+f32 fn_800D04AC(int nPlayer);
+f32 fn_800D04E0(int nPlayer);
+f32 fn_800D0478(int nPlayer) {
+    return fn_800D05A4(gPlayers[nPlayer].ball.vPos);
 }
 
-void fn_800D04AC(s32 p0) {
-    fn_800D05A4(((gPlayers + (p0 * 3832)) + 0xAD0), (p0 * 3832));
+f32 fn_800D04AC(int nPlayer) {
+    return fn_800D05A4(gPlayers[nPlayer].ball.vStart);
 }
 
-void fn_800D04E0(s32 p0) {
-    fn_800D05A4(((gPlayers + (p0 * 3832)) + 0xB5C), (p0 * 3832));
+f32 fn_800D04E0(int nPlayer) {
+    return fn_800D05A4(gPlayers[nPlayer].ballBefore.vPos);
 }
