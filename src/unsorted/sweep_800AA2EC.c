31d255d1ff0ec1f6b5faf9f4d3fb9b33c332ecd8
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-23 16:54:34 -0500

    sweep_800AA2EC.c: give fn_800AA444 its typed prototype

--- src/unsorted/sweep_800AA2EC.c
+++ src/unsorted/sweep_800AA2EC.c
@@ -2,19 +2,19 @@
 
 #include "game_types.h"
 
-void fn_800AA444();
+void fn_800AA444(u8* p, u8 v);
 void fn_800AB118();
 void fn_800AB14C();
 
 void fn_800AA2EC(void);
-void fn_800AA30C(void);
+void fn_800AA30C(u8* p, u8 v);
 void fn_800AA32C(void);
 void fn_800AA2EC(void) {
     fn_800AB118();
 }
 
-void fn_800AA30C(void) {
-    fn_800AA444();
+void fn_800AA30C(u8* p, u8 v) {
+    fn_800AA444(p, v);
 }
 
 void fn_800AA32C(void) {
