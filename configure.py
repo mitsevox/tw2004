d407a80306e878bc91ac6a2ca226761f53a22115
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-23 14:29:15 -0500

    GCN_mem_alloc.c: linked (.rodata range)

--- configure.py
+++ configure.py
@@ -445,7 +445,7 @@ config.libs = [
         "src_dir": "extern/ffcc",
         "objects": [
             Object(Matching, "src/Runtime.PPCEABI.H/global_destructor_chain.c"),
-            Object(NonMatching, "src/Runtime.PPCEABI.H/GCN_mem_alloc.c"),
+            Object(Matching, "src/Runtime.PPCEABI.H/GCN_mem_alloc.c"),
             Object(NonMatching, "src/MSL_C/PPCEABI/bare/H/abort_exit.c"),
             Object(NonMatching, "src/MSL_C/PPCEABI/bare/H/alloc.c"),
             Object(NonMatching, "src/MSL_C/PPCEABI/bare/H/ansi_files.c"),
