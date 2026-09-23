5bf8c40b5440e659250d196a3fa0ee6b57c57bb2
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-23 13:55:45 -0500

    OSArena.c: linked (.sdata, .sbss ranges)

--- configure.py
+++ configure.py
@@ -743,7 +743,7 @@ config.libs = [
             Object(NonMatching, "dolphin/os/OSContext.c"),
             Object(NonMatching, "dolphin/os/OSReboot.c"),
             Object(Matching, "dolphin/os/OSAlarm.c"),
-            Object(NonMatching, "dolphin/os/OSArena.c"),
+            Object(Matching, "dolphin/os/OSArena.c"),
             Object(NonMatching, "dolphin/os/OSCache.c"),
             Object(Matching, "dolphin/os/OSReset.c"),
             Object(Matching, "dolphin/os/OSRtc.c"),
