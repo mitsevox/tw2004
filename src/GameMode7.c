3523d5d574c6f0b0df9e71aa5d82bc3e43396601
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-23 07:48:04 -0500

    GameMode7.c: new unit (0x800F9610-0x800F986C), 2 sweep files folded in

--- src/GameMode7.c
+++ src/GameMode7.c
@@ -1,11 +1,15 @@
-// Small functions found by the sweep (sweep.py). Original file and meanings unknown.
-
 #include "game_types.h"
 
+void fn_800FA570();
+void fn_800F9824(void);
 extern u8 lbl_802823C9;
 void fn_800F9AB0();
-
 void fn_800F9844(void);
+
+void fn_800F9824(void) {
+    fn_800FA570();
+}
+
 void fn_800F9844(void) {
     lbl_802823C9 = 1;
     fn_800F9AB0();
