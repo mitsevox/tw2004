afe55fc9634c92f31d35b4deb09c4daa2d810b92
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-23 17:38:40 -0500

    sweep_800394AC, sweep_80039520, sweep_8003DC78: header prototypes, stream ids as 'CAMS'/'CAMV'/'CAMA'

--- src/unsorted/sweep_80039520.c
+++ src/unsorted/sweep_80039520.c
@@ -1,12 +1,11 @@
 // Small functions found by the sweep (sweep.py). Original file and meanings unknown.
 
-#include "game_types.h"
+#include "engine.h"
 
-void UStream_UnregisterHandler();
-void fn_80039550();
+void fn_80039550(void);
 
 void fn_80039520(void);
 void fn_80039520(void) {
-    UStream_UnregisterHandler(1128353110);
+    UStream_UnregisterHandler('CAMV');
     fn_80039550();
 }
--- src/unsorted/sweep_8003DC78.c
+++ src/unsorted/sweep_8003DC78.c
@@ -1,14 +1,11 @@
 // Small functions found by the sweep (sweep.py). Original file and meanings unknown.
 
-#include "game_types.h"
+#include "camera.h"
 
+u8 fn_8003DC78(CamShot* pShot) {
+    u8 nKind = pShot->bAC;
 
-s32 fn_8003DC78(void* arg0);
-s32 fn_8003DC78(void* arg0) {
-    u8 temp_r3;
-
-    temp_r3 = (*(u8*)((u8*)(arg0) + 0xAC));
-    if ((temp_r3 == 1) || ((u8) (temp_r3 - 2) <= 4U) || (temp_r3 == 7)) {
+    if (nKind == 1 || (u8)(nKind - 2) <= 4U || nKind == 7) {
         return 1;
     }
     return 0;
