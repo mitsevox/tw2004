596f825a1b9abc3b4aeaccff61d1f5cdc3e6e49e
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-23 10:35:49 -0500

    dolphin/os/OSTime.c linked as Matching

--- configure.py
+++ configure.py
@@ -757,7 +757,7 @@ config.libs = [
             Object(NonMatching, "dolphin/os/OSCache.c"),
             Object(NonMatching, "dolphin/os/OSReset.c"),
             Object(Matching, "dolphin/os/OSRtc.c"),
-            Object(NonMatching, "dolphin/os/OSTime.c"),
+            Object(Matching, "dolphin/os/OSTime.c"),
             Object(Matching, "dolphin/PPCArch.c"),
             Object(Matching, "dolphin/os/OSResetSW.c"),
             Object(Matching, "dolphin/os/OSInterrupt.c"),
