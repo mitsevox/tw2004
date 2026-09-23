22b5d3fe1f8fd588d8dc4a3a0d03b19efff34dce
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-23 18:45:56 -0500

    sweeps 0x80070BD0/C7C/D28: use gx.h's GXSetArray

--- src/unsorted/sweep_80070C7C.c
+++ src/unsorted/sweep_80070C7C.c
@@ -1,14 +1,14 @@
 // Small functions found by the sweep (sweep.py). Original file and meanings unknown.
 
 #include "game_types.h"
+#include "gx.h"
 
-s32 GXSetArray(s32, s32, s32);
 s32 fn_80070168(s32, s32);
 
 void fn_80070C7C(void* arg0);
 void fn_80070C7C(void* arg0) {
     if ((u8) (*(u8*)((u8*)(arg0) + 0xC)) != 0) {
-        GXSetArray(9, *(*(s32**)((u8*)(arg0) + 0x24)), 0xC);
+        GXSetArray(9, *(*(void***)((u8*)(arg0) + 0x24)), 0xC);
     }
     fn_80070168((*(s32*)((u8*)(arg0) + 8)), (*(s32*)((u8*)(arg0) + 4)));
 }
