5a9a2c97dd807d3e11a736581eb83f6185b822ba
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-24 05:02:37 -0500

    LLTex.c: fn_8000EA1C 98.4 -> 98.9 (EA's abs)

--- src/LLTex.c
+++ src/LLTex.c
@@ -238,10 +238,8 @@ int fn_8000EA1C(u8* pCode, int nArg, int nPush, DynObj* pObj) {
                     a %= 32;
                     a = lbl_80281C70[a];
                 } else {
-                    b = (int)Rand_Next(0);
-                    if (b < 0) {
-                        b = -b;
-                    }
+                    int nRand = (int)Rand_Next(0);
+                    b = (nRand + (nRand >> 31)) ^ (nRand >> 31);   // EA's abs
                     a = b % -a;
                 }
                 break;
