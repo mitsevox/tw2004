86fbd5fe7de1760fed2a6a879ca2dc247d61f24f
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-23 21:43:48 -0500

    Fill 0x800AE1F8: uiobject.c UI_Obj_InitModule draft (92%)

--- include/uiobject.h
+++ include/uiobject.h
@@ -18,6 +18,10 @@ typedef struct UIObjSettings {
 LAYOUT_ASSERT(UIObjSettings, 0x70);
 
 extern UIObjSettings lbl_801F5B98;
+extern TexBank*   lbl_802820BC; // the bank of the textures below
+extern TexEntry*  lbl_802820C0; // "ring"
+extern TexEntry*  lbl_802820C4; // } "toball", both
+extern TexEntry*  lbl_802820C8; // }
 extern f32        lbl_801F5B78[8];
 extern UObject*   lbl_802820D0; // made from the 'TEO ' object 10003
 extern CamLens*   lbl_802820CC; // the objects' lens (fn_80076400)
