87ec9917ca9f52c90a907852a9a5113706f31572
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-24 13:21:57 -0500

    FE_CrAPDB.c linked (.data, .sdata, .sbss, .sdata2 ranges)

--- configure.py
+++ configure.py
@@ -1001,7 +1001,7 @@ config.libs = [
             Object(Matching, "GameMode11.c"),
             Object(Matching, "GameMode4.c"),
             Object(NonMatching, "uiArc.c"),
-            Object(NonMatching, "FE_CrAPDB.c"),
+            Object(Matching, "FE_CrAPDB.c"),
             Object(Matching, "FE_CrAPMessages.c"),
             Object(NonMatching, "LLDynTex.c"),
             Object(NonMatching, "GameMode26.c"),
