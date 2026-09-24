26084cbaf9dfbf36397552f04b0e125021d6daba
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-23 22:37:27 -0500

    Fill 0x800B2DB0: shadow.c fn_800B2DB0 exact (16/19)

--- src/shadow.c
+++ src/shadow.c
@@ -20,6 +20,7 @@ void fn_800B2724(void);
 void fn_800B2DB0(f32* pCentre, f32 (*pQuad)[4], f32 fHalfX, f32 fHalfZ);
 void fn_800140E8(int a, int nWidth, int nHeight, int nField, int b, int c);
 void fn_80035294(void);                 // GoTerrain.c
+void fn_800352BC(void);
 void fn_80036100(u8* pMesh, void* pDesc, int n);
 void fn_800360D4(u8* pMesh);
 
@@ -185,6 +186,67 @@ void fn_800B281C(void) {
     aXY[6] = 0.0f;
     fn_80014194(aColour);
     fn_8001644C(0xA1, aXY, 0, NULL, 2);
+}
+
+// The flat shadow: the square pQuad (four corners around pCentre, 2*fHalfX by 2*fHalfZ) as one
+// strip, lifted by lbl_802814B0 and mapped onto the whole shadow texture. pCentre is moved to the
+// square's far corner.
+void fn_800B2DB0(f32* pCentre, f32 (*pQuad)[4], f32 fHalfX, f32 fHalfZ) {
+    s16           aIndex[4] = {0, 1, 2, 3};
+    u32           aColour[4];
+    TrailMeshDesc desc;
+    f32           aUV[4][2];
+    f32           aPos[4][3];
+    Shadow*       p = lbl_802814A8;
+    f32           fScaleX;
+    f32           fScaleZ;
+    f32*          pUV;
+    u32*          pColour;
+    f32*          pPos;
+    int           i;
+
+    pCentre[0] += fHalfX;
+    pCentre[2] += fHalfZ;
+    fScaleX = 1.0f / (2.0f * fHalfX);
+    fScaleZ = 1.0f / (2.0f * fHalfZ);
+    pPos = aPos[0];
+    pColour = aColour;
+    pUV = aUV[0];
+    for (i = 0; i < 4; i++, pPos += 3, pUV += 2, pColour++) {
+        pPos[0] = pQuad[i][0];
+        pPos[1] = lbl_802814B0 + pQuad[i][1];
+        pPos[2] = pQuad[i][2];
+        // EA bug: a fourth coordinate, written past each vertex (the next vertex overwrites it;
+        // the last one lands past the array)
+        pPos[3] = 1.0f;
+        pUV[0] = fScaleX * (pCentre[0] - pPos[0]);
+        pUV[1] = fScaleZ * (pCentre[2] - pPos[2]);
+        *pColour = 0x80000000;
+    }
+    fn_80035240(lbl_80281EE0->mC0);
+    fn_800352BC();
+    fn_80013CCC(fn_8001614C());
+    fn_80014118(0x70);
+    fn_8002A608(&p->tex);
+    fn_80016B9C();
+    fn_80035138(0);
+    fn_80012F50(0, 6, 0x80);
+    fn_80012F18(7);
+    fn_80012F34(0);
+    fn_80035118(4, 5);
+    fn_80012EF8();
+    desc.n0 = 4;
+    desc.nVerts = 4;
+    desc.pDraw = NULL;
+    desc.pIndices = aIndex;
+    desc.pPos = aPos[0];
+    desc.pColour = (u8*)aColour;
+    desc.pUV = aUV[0];
+    fn_80036100(p->aMesh[0], &desc, 1);
+    fn_800360D4(p->aMesh[0]);
+    fn_80012F50(1, 6, 0x80);
+    fn_80012F18(6);
+    fn_80012F34(1);
 }
 
 // Draw the golfer's shadow texture onto the ground: a square 2*f424 by 2*f428 around v414. When
