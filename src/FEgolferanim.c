13a3f31da793d9b8689d7dee6bc1d2e6ef264875
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-24 02:22:22 -0500

    Fill 0x8008B044: FEgolferanim.c fn_8008B044, fn_8008E0B0 exact (90/93)

--- src/FEgolferanim.c
+++ src/FEgolferanim.c
@@ -219,8 +219,6 @@ void fn_8008B00C(void) {
 // Show golfer nGolfer: the one shown, the next or the previous one if one of them is him, else
 // load him into the shown slot. Golfers 7 and 29 (with a or b) set b90.
 void fn_8008B044(int nGolfer, int a, int b) {
-    CrAPGolfer* pGolfer;
-
     fn_8008B704();
     fn_8008B754(1);
     if (nGolfer == 7 || nGolfer == 29) {
@@ -234,16 +232,15 @@ void fn_8008B044(int nGolfer, int a, int b) {
     if (a == 7 || a == 29) {
         lbl_80281EE0->b90 = 1;
     }
-    pGolfer = lbl_80281EE0->pB4;
-    if (pGolfer->nC != nGolfer) {
-        if (pGolfer->pNext->nC == nGolfer) {
-            lbl_80281EE0->pB4 = pGolfer->pNext;
+    if (lbl_80281EE0->pB4->nC != nGolfer) {
+        if (lbl_80281EE0->pB4->pNext->nC == nGolfer) {
+            lbl_80281EE0->pB4 = lbl_80281EE0->pB4->pNext;
             gPlayers[0].pChar = lbl_80281EE0->pB4->pChar;
-        } else if (pGolfer->pPrev->nC == nGolfer) {
-            lbl_80281EE0->pB4 = pGolfer->pPrev;
+        } else if (lbl_80281EE0->pB4->pPrev->nC == nGolfer) {
+            lbl_80281EE0->pB4 = lbl_80281EE0->pB4->pPrev;
             gPlayers[0].pChar = lbl_80281EE0->pB4->pChar;
         } else {
-            pGolfer->nC = nGolfer;
+            lbl_80281EE0->pB4->nC = nGolfer;
             lbl_80281EE0->pB4->b18 = 0;
             lbl_80281EE0->pB4->n1C = -1;
         }
@@ -1368,9 +1365,9 @@ void fn_8008E0B0(f32 fTurn) {
     fDiff = fabsf(lbl_80281EE0->f1A0 - lbl_80281EE0->f19C);
     if (fDiff < 2.0f * PI - fDiff) {
         if (lbl_80281EE0->f19C < lbl_80281EE0->f1A0 - 0.05f) {
-            lbl_80281EE0->f19C = lbl_80281EE0->f19C + 0.05f;
+            lbl_80281EE0->f19C += 0.05f;
         } else if (lbl_80281EE0->f19C > 0.05f + lbl_80281EE0->f1A0) {
-            lbl_80281EE0->f19C = lbl_80281EE0->f19C - 0.05f;
+            lbl_80281EE0->f19C -= 0.05f;
         }
     } else if (lbl_80281EE0->f19C < lbl_80281EE0->f1A0) {
         lbl_80281EE0->f19C = lbl_80281EE0->f19C - 0.05f;
@@ -1378,7 +1375,7 @@ void fn_8008E0B0(f32 fTurn) {
             lbl_80281EE0->f19C = 2.0f * PI + lbl_80281EE0->f19C;
         }
     } else if (lbl_80281EE0->f19C > lbl_80281EE0->f1A0) {
-        lbl_80281EE0->f19C = lbl_80281EE0->f19C + 0.05f;
+        lbl_80281EE0->f19C += 0.05f;
         if (lbl_80281EE0->f19C > 2.0f * PI) {
             lbl_80281EE0->f19C = lbl_80281EE0->f19C - 2.0f * PI;
         }
