114086299b3939ebee4974a6cda6530c8f515c35
mitsevox <156822169+mitsevox@users.noreply.github.com>
2026-09-24 04:17:42 -0500

    SkinPart.c: fn_800CDF80 line split (lint)

--- src/SkinPart.c
+++ src/SkinPart.c
@@ -838,7 +838,8 @@ void fn_800CDF80(Skin* pSkin, int nPart, int nVariant, int nLink) {
     if (pSkin == NULL) return;
     pDesc = pSkin->pModel->pDesc;
     // fake match: the cast, as in fn_800CCAC0
-    pLink = &pDesc->pLinks[nLink + ((SkinVariant*)pDesc->pVariants)[nVariant + pDesc->pParts[nPart].nFirst].nFirstLink];
+    pLink = &pDesc->pLinks[((SkinVariant*)pDesc->pVariants)[nVariant + pDesc->pParts[nPart].nFirst].nFirstLink
+                           + nLink];
     nOther = fn_800CDAFC(pSkin, pLink->uPart);
     if (nOther >= 0 && nOther < fn_800CCA40(pSkin)) {
         fn_800CCC1C(pSkin, nOther, pLink->nOption);
